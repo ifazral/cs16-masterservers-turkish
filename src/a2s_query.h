eddb77cea80714da5a7d8157bcd6232b9d65eeb1
ifazral <ifazralgames@gmail.com>
2026-08-09 05:14:52 +0300

    Update a2s_query.h

--- src/a2s_query.h
+++ src/a2s_query.h
@@ -1,29 +1,26 @@
-#ifndef A2S_QUERY_H
-#define A2S_QUERY_H
-
+#pragma once
 #include <stdint.h>
-#include <stddef.h>
 
 #pragma pack(push, 1)
 struct a2s_server_info_t
 {
-    char name[256];
-    char map[256];
-    char gamedir[256];
-    char gamedesc[256];
-    uint16_t appid;
-    uint8_t players;
-    uint8_t max_players;
-    uint8_t bots;
-    char type;
-    char os;
-    uint8_t password;
-    uint8_t secure;
-    char version[64];
-    uint32_t ip;
-    uint16_t port;
-    int ping_ms;
-    bool valid;
+	char name[256];
+	char map[256];
+	char gamedir[256];
+	char gamedesc[256];
+	uint16_t appid;
+	uint8_t players;
+	uint8_t max_players;
+	uint8_t bots;
+	char type;
+	char os;
+	uint8_t password;
+	uint8_t secure;
+	char version[64];
+	uint32_t ip;
+	uint16_t port;
+	int ping_ms;
+	bool valid;
 };
 #pragma pack(pop)
 
@@ -34,6 +31,4 @@ bool parse_a2s_response(const uint8_t *data, int len, a2s_server_info_t *out);
 bool a2s_query_server(uint32_t ip_net, uint16_t port_net, a2s_server_info_t *out, int timeout_ms = 2000);
 
 int a2s_query_batch(uint32_t *ips, uint16_t *ports, int count,
-    a2s_server_info_t *results, int timeout_ms = 3000);
-
-#endif // A2S_QUERY_H
+	a2s_server_info_t *results, int timeout_ms = 3000);
